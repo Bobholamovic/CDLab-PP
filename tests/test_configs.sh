325048dedd246cf49a5615c6f6427f76a6d5f9dc
Bobholamovic <bob1998425@hotmail.com>
2021-12-28 21:52:48 +0800

    test: add full config test

--- tests/test_configs.sh
+++ tests/test_configs.sh
@@ -9,6 +9,8 @@ python train.py train --exp_config ../configs/levircd/config_levircd_cdnet.yaml 
 python train.py train --exp_config ../configs/levircd/config_levircd_siamunet-conc.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
 python train.py train --exp_config ../configs/levircd/config_levircd_siamunet-diff.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
 python train.py train --exp_config ../configs/levircd/config_levircd_unet.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
+python train.py train --exp_config ../configs/levircd/config_levircd_ifn.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
+python train.py train --exp_config ../configs/levircd/config_levircd_snunet.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
 
 # OSCD
 python train.py train --exp_config ../configs/oscd/config_oscd_siamunet-conc.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
@@ -20,6 +22,8 @@ python train.py train --exp_config ../configs/svcd/config_svcd_cdnet.yaml --batc
 python train.py train --exp_config ../configs/svcd/config_svcd_siamunet-conc.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
 python train.py train --exp_config ../configs/svcd/config_svcd_siamunet-diff.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
 python train.py train --exp_config ../configs/svcd/config_svcd_unet.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
+python train.py train --exp_config ../configs/svcd/config_svcd_ifn.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
+python train.py train --exp_config ../configs/svcd/config_svcd_snunet.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
 
 # AirChange-Szada
 python train.py train --exp_config ../configs/szada/config_szada_cdnet.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
@@ -38,3 +42,5 @@ python train.py train --exp_config ../configs/whu/config_whu_cdnet.yaml --batch_
 python train.py train --exp_config ../configs/whu/config_whu_siamunet-conc.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
 python train.py train --exp_config ../configs/whu/config_whu_siamunet-diff.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
 python train.py train --exp_config ../configs/whu/config_whu_unet.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
+python train.py train --exp_config ../configs/whu/config_whu_ifn.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
+python train.py train --exp_config ../configs/whu/config_whu_snunet.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
