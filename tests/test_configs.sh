6a1934ebb641c5dbc8380c158b86623fd2dfc7e0
Bobholamovic <bob1998425@hotmail.com>
2021-12-31 21:48:14 +0800

    test: update test

--- tests/test_configs.sh
+++ tests/test_configs.sh
@@ -12,6 +12,8 @@ python train.py train --exp_config ../configs/levircd/config_levircd_unet.yaml -
 python train.py train --exp_config ../configs/levircd/config_levircd_ifn.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
 python train.py train --exp_config ../configs/levircd/config_levircd_snunet.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
 python train.py train --exp_config ../configs/levircd/config_levircd_stanet.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
+python train.py train --exp_config ../configs/levircd/config_levircd_lunet.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
+python train.py train --exp_config ../configs/levircd/config_levircd_p2v.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
 
 # OSCD
 python train.py train --exp_config ../configs/oscd/config_oscd_siamunet-conc.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
@@ -26,6 +28,10 @@ python train.py train --exp_config ../configs/svcd/config_svcd_unet.yaml --batch
 python train.py train --exp_config ../configs/svcd/config_svcd_ifn.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
 python train.py train --exp_config ../configs/svcd/config_svcd_snunet.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
 python train.py train --exp_config ../configs/svcd/config_svcd_stanet.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
+python train.py train --exp_config ../configs/svcd/config_svcd_lunet.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
+python train.py train --exp_config ../configs/svcd/config_svcd_p2v.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
+python train.py train --exp_config ../configs/svcd/config_svcd_dsamnet.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
+python train.py train --exp_config ../configs/svcd/config_svcd_bit.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
 
 # AirChange-Szada
 python train.py train --exp_config ../configs/szada/config_szada_cdnet.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
@@ -47,3 +53,5 @@ python train.py train --exp_config ../configs/whu/config_whu_unet.yaml --batch_s
 python train.py train --exp_config ../configs/whu/config_whu_ifn.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
 python train.py train --exp_config ../configs/whu/config_whu_snunet.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
 python train.py train --exp_config ../configs/whu/config_whu_stanet.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
+python train.py train --exp_config ../configs/whu/config_whu_lunet.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
+python train.py train --exp_config ../configs/whu/config_whu_p2v.yaml --batch_size 1 --num_epochs 1 --num_workers 0 --repeats 1 --log_off --debug_on
