fe26b8e1b74bbd9970cc94c3bc51a17d1edc14a4
Bobholamovic <bob1998425@hotmail.com>
2021-12-31 21:47:31 +0800

    refactor: change method order in STANetTrainer

--- src/impl/trainers/stanet_trainer.py
+++ src/impl/trainers/stanet_trainer.py
@@ -4,11 +4,11 @@ from .cd_trainer import CDTrainer
 
 
 class STANetTrainer(CDTrainer):
-    def _prepare_data(self, t1, t2, tar):
-        return t1, t2, tar.astype('float32')
-        
     def _init_trainer(self):
         self.thresh = self.ctx['threshold']
+
+    def _prepare_data(self, t1, t2, tar):
+        return t1, t2, tar.astype('float32')
 
     def _pred_to_prob(self, pred):
         return (pred>self.thresh).astype('float32')
